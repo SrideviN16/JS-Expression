b3147a59223486611f9e09ad39fe7caf91c12cae
SrideviN16 <sridevi16devi97@gmail.com>
2023-03-24 22:44:09 +0530

    update

---
+++ main.js
@@ -0,0 +1,67 @@
+//addition 
+var a=10;
+var b=20;
+console.log(a+b);
+//subtraction 
+var c=40;
+var d=80;
+console.log(d-c);
+// multipication
+var e=4;
+var f=20;
+console.log(e*f);
+//division 
+var g=90;
+var f=3;
+console.log(g/f);
+// circle area 
+var py=22/7;
+var r=7;
+var ac=py*r*r;
+console.log(ac);
+// area of traiangle 
+var b1=7;
+var h1=8;
+var at=1/2*b1*h1;
+console.log(at);
+// simple interest 
+var p=4000;
+var n=1;
+var r=10;
+var si=p*n*r/100;
+console.log(si);
+//square 
+var s=4;
+console.log(s**2);
+//cube 
+var cb=5;
+console.log(cb**3);
+// totel and avg 
+var s1=80;
+var s2=80;
+var s3=90;
+var totel=s1+s2+s3;
+var avg=totel/3;
+console.log(totel);
+console.log(avg);
+//larger number 
+var l=20;
+var s=30;
+console.log(l>s);
+//positive number 
+var v=-4;
+console.log(v>0);
+//number even 
+var evn=27;
+var ans=(evn%2);
+console.log(ans==0);
+//greater than 10 
+ var gt=20;
+ console.log(gt>10);
+ //number dvd by 5 
+ var df=20;
+ var adf=df/5;
+ console.log(adf>0);
+
+ 
+
